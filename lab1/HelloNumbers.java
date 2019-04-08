df1bb88e8eabdb08865340a0295e356c61ef5000
pin <elliephc@gmail.com>
2019-04-08 15:27:35 +0800

    lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,15 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int sum = 0;
+        String y = 4 + "horse";
+        while (x < 10) {
+            sum += x;
+            System.out.print(sum + " ");
+            System.out.print(3 + "10");
+            x = x + 1;
+
+        }
+        System.out.println(y);
+    }
+}
