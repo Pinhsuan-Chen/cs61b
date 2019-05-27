45a77fd74ea8a23e05df7622a152c827fda46958
pin <elliephc@gmail.com>
2019-05-27 17:12:18 +0800

    [don't know which years ] files

---
+++ dont_know_which_year/lab2setup/Dog.java
@@ -0,0 +1,18 @@
+public class Dog {
+    private int size;
+
+    public Dog(int s) {
+        size = s;
+    }
+
+    /** Makes a noise. */
+    public String noise() {
+        if (size < 10) {
+            return "yip";
+        } 
+        return "bark";
+    }
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
---
+++ dont_know_which_year/lab2setup/small/TestSort.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.assertEquals;
+public class TestSort {
+    /** Tests the sort method of the Sort class. */
+    public static void testSort() {
+        String[] input = {"i", "have", "an", "egg"};
+        String[] expected = {"an", "egg", "have", "i"};
+        Sort.sort(input);
+        for (int i = 0; i < input.length; i += 1) {
+            if (!input[i].equals(expected[i])) {
+                System.out.println("Mismatch in position " + i + ", expected: " + expected + ", but got: " + input[i] + ".");
+                break;
+            }
+        }
+    }
+    public static void testFindSmallest() {
+        String[] input = {"i", "have", "an", "egg"};
+        String expected = "an";
+
+        String actual = Sort.findSmallest(input);
+        assertEquals("opps",expected, actual);
+    }
+
+    public static void main(String[] args) {
+        testFindSmallest();
+        System.out.println("hii");
+    }
+}
